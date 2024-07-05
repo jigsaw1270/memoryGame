b5e8ad3ac147fba1963c92814c8fba9a4119ec3a
Mahmudul <mahmudul@blubnc.com>
2024-07-05 18:03:39 +0600

    first  commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const section  =  document.querySelector('section');
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+//lik text
+ playerLivesCount.textContent =  playerLives;
+
+ //generate the data
+ [{imgSrc : './images/beatles.jpeg'}]
+
+const getData = () => [
+    { imgSrc: "./images/beatles.jpeg", id: 1, name: "beatles" },
+    { imgSrc: "./images/blink182.jpeg", id: 2, name: "blink 182" },
+    { imgSrc: "./images/fkatwigs.jpeg", id: 3, name: "fka twigs" },
+    { imgSrc: "./images/fleetwood.jpeg", id: 4, name: "fleetwood" },
+    { imgSrc: "./images/joy-division.jpeg", id: 5, name: "joy division" },
+    { imgSrc: "./images/ledzep.jpeg", id: 6, name: "lep zeppelin" },
+    { imgSrc: "./images/metallica.jpeg", id: 7, name: "metallica" },
+    { imgSrc: "./images/pinkfloyd.jpeg", id: 8, name: "pink floyd" },
+    { imgSrc: "./images/beatles.jpeg", id: 9, name: "beatles" },
+    { imgSrc: "./images/blink182.jpeg", id: 10, name: "blink 182" },
+    { imgSrc: "./images/fkatwigs.jpeg", id: 11, name: "fka twigs" },
+    { imgSrc: "./images/fleetwood.jpeg", id: 12, name: "fleetwood" },
+    { imgSrc: "./images/joy-division.jpeg", id: 13, name: "joy division" },
+    { imgSrc: "./images/ledzep.jpeg", id: 14, name: "led zeppelin" },
+    { imgSrc: "./images/metallica.jpeg", id: 15, name: "metallica" },
+    { imgSrc: "./images/pinkfloyd.jpeg", id: 16, name: "pink floyd" },];
+
+
+
+    //randomize 
+    const  randomize = () => {
+        const cardData = getData ();
+        console.log(cardData);
+        cardData.sort(()=> Math.random() - 0.5);
+        return cardData;
+
+    };
+    randomize();
+
+
+    //card generator 
+    const  cardGenerator = () => {
+        const  cardData = randomize();
+
+        cardData.forEach(item =>{
+            console.log(item);
+            const  card = document.createElement("div");
+            const  face =  document.createElement('img');
+            const  back = document .createElement("div");
+            card.classList =  "card" ;
+            face.classList = "face";
+            back.classList = "back";
+            //attach the infor  for the card 
+            face.src = item.imgSrc;
+
+            //attach the  cards  to  the section
+
+            section.appendChild(card);
+            card.appendChild(face);
+            card.appendChild(back);
+        });
+   
+    };
+
+    cardGenerator();
