e7b66e82d03d4cf37f7a3f9328710a7c492f3bcf
jigsaw1270 <nomadshomoy@gmail.com>
2024-07-06 15:40:45 +0600

    game  mechanics  done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const section  =  document.querySelector('section');
 const playerLivesCount = document.querySelector("span");
-const playerLives = 6;
+let playerLives = 6;
 
 //lik text
  playerLivesCount.textContent =  playerLives;
@@ -43,7 +43,9 @@ const getData = () => [
     const  cardGenerator = () => {
         const  cardData = randomize();
 
-        cardData.forEach(item =>{
+
+       
+        cardData.forEach((item)=>{
             console.log(item);
             const  card = document.createElement("div");
             const  face =  document.createElement('img');
@@ -53,14 +55,83 @@ const getData = () => [
             back.classList = "back";
             //attach the infor  for the card 
             face.src = item.imgSrc;
+            card.setAttribute('name', item.name);
 
             //attach the  cards  to  the section
 
             section.appendChild(card);
             card.appendChild(face);
             card.appendChild(back);
+
+
+            card.addEventListener('click', (e)=> {
+                card.classList.toggle("toggleCard");
+                checkCards(e);
+            })
         });
    
+    };
+
+    //check cards 
+    const checkCards = (e) => {
+        const clickedCard = e.target; 
+        clickedCard.classList.add('flipped');
+        const flippedCards =  document.querySelectorAll('.flipped');
+        const toggleCard = document.querySelectorAll('.toggleCard')
+       
+
+        //logic
+        if(flippedCards.length ===2){
+            if(flippedCards[0].getAttribute('name')=== flippedCards[1].getAttribute('name')){
+                console.log("match");
+                flippedCards.forEach(card=>{
+                    card.classList.remove("flipped");
+                    card.style.pointerEvents = "none";
+                })
+            } else{
+                console.log("wrong");
+                flippedCards.forEach(card =>{
+                    card.classList.remove('flipped');
+                    setTimeout(()=>card.classList.remove('toggleCard'),1000); 
+                });
+                playerLives--;
+                playerLivesCount.textContent = playerLives;
+                if(playerLives === 0){
+                    restart("Damn , u lost  kid!!");
+                }
+            }
+        }
+    {
+
+    }
+    //if we  won
+
+    if(toggleCard.length === 16){
+        restart("congratulation , u just  beat the  game")
+    }
+        
+    };
+
+    //reset 
+
+    const restart = (text) => {
+        let cardData = randomize();
+        let  faces = document.querySelectorAll('.face');
+        let cards =  document.querySelectorAll('.card');
+        section.style.pointerEvents = "none";
+        cardData.forEach((item,index )=>{
+            cards[index].classList.remove('toggleCard');
+            //randomize
+            setTimeout(()=>{
+                cards[index].style.pointerEvents ="all"; 
+           faces[index].src = item.imgSrc;
+            cards[index].setAttribute("name", item.name);
+            section.style.pointerEvents = "all";
+            }, 1000);
+        });
+        playerLives = 6;
+        playerLivesCount.textContent= playerLives;
+        setTimeout(()=> window.alert(text), 100);
     };
 
     cardGenerator();
